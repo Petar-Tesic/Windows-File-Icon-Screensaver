30741276eaa42526d50fd2dc517e1cc7e2917b42
Petar-Tesic <survey03.tesic@gmail.com>
2025-04-26 18:28:34 +0300

    Icons.py

--- Icons.py
+++ Icons.py
@@ -8,6 +8,7 @@ import platform
 
 IS_WINDOWS = platform.system() == 'Windows'
 #I named the functions as if i am 7 and i want to act serious🥀
+#Same for Dicts😭
 if IS_WINDOWS:
     import win32gui
     import win32ui
